a6784ddf31f345500ecb851a93372e03d9d409a1
1Lubo <1Lubo@pm.me>
2022-03-30 15:27:54 +0200

    add game save and load

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 ULS = "\e[4m" # underline start
 ULE = "\e[24m" # underline end
 GREEN = "\e[32m"
@@ -22,6 +24,9 @@ end
 class Game
     attr_accessor :word, :attempts_left, :letters_guessed, :word_to_letters, :win
 
+    SaveFolderName = '../saves/'
+    SaveName = 'save.txt'
+
     RULES = "\n#{BOLDS}Hangman is a guessing game. The computer will pick a word between 5-10 characters and you will try to guess it by suggesting letters. You have 12 chances to guess incorectly. #{BOLDE}"
 
     include Display
@@ -32,6 +37,18 @@ class Game
         @letters_guessed = []
         @attempts_left = 12
         @win = false
+    end
+
+    def deserialize(string)
+        game_hash = JSON.parse(string)
+    end
+
+    def serialize
+        {'word' => self.word, 'to_letters' => self.word_to_letters, 'guessed' => self.letters_guessed, 'attempts' => self.attempts_left}.to_json
+    end
+
+    def to_s
+        "Game: Word: #{self.word} letters guessed: #{letters_guessed}. Attempts left: #{attempts_left}"
     end
 
     def attempts_left
@@ -69,7 +86,7 @@ class Game
     def get_next_letter
         letter = 0
         while letter.ord < 97 || letter.ord > 122
-            puts "Please enter a letter."
+            puts "Please enter a letter. \t (Enter 'save' to save the game and exit)"
             letter = gets.chomp.downcase
             if self.letters_guessed.include?(letter)
                 puts "You've already tried this"
@@ -77,6 +94,9 @@ class Game
                 next
             elsif letter.ord == 32
                 next 
+            elsif letter == 'save'
+                self.save_game
+                return letter
             end
         end
 
@@ -114,35 +134,84 @@ class Game
     def start
         
         want_to_play = ''
-        acceptable_answers = ['y', 'n']
+        acceptable_answers = ['yes', 'no', 'load']
         puts "Would you like to play hangman?"
+        puts "Please answer: \n'yes' - to start a new game \n'load' - to load a saved game \n'no' - to quit"  
 
         while !acceptable_answers.include?(want_to_play )
-            puts "Please answer y / n"    
+            puts "Please choose one of the above options"  
             want_to_play = gets.chomp.downcase
         end
 
-        if want_to_play == 'y'
-            true
+        want_to_play
+    end
+
+    def save_game
+
+        if Dir.exist?(SaveFolderName)
+            puts "Save folder already exists. Saving file"            
         else
-            false
+            puts "Save folder does not exist"
+            puts "Creating save folder"            
+            Dir.mkdir(SaveFolderName)
+        end
+
+        File.open("#{SaveFolderName}#{SaveName}", "w"){ |somefile| somefile.puts self.serialize }
+    end
+
+    def load_game
+        
+        if File.exist?("#{SaveFolderName}#{SaveName}")
+            
+            contents = File.open("#{SaveFolderName}#{SaveName}", "r"){ |file| file.read }
+            return self.deserialize(contents)
+        else
+            puts `clear`
+            puts "#{BOLDS}No save file exists yet#{BOLDE}"
+            puts
+            return false
         end
     end
 
     def play()
         start = self.start
 
-        if !start
-            puts
-            puts "#{BOLDS}See ya next time#{BOLDE}"
+        if start == 'no'
+            puts `clear`
+            puts "#{BOLDS}See ya next time!#{BOLDE}"
             return
         end
         
-        puts RULES
+        if start == 'yes'
+            puts `clear`
+            puts RULES
+        end
+        
+        if start == 'load'
+            saved_game = self.load_game
+
+            if !saved_game
+                self.play
+            else
+                
+                self.word = saved_game.fetch('word')
+                self.attempts_left = saved_game.fetch('attempts')
+                self.word_to_letters = saved_game.fetch('to_letters')
+                self.letters_guessed = saved_game.fetch('guessed')
+                
+            end
+        end
 
         while self.attempts_left > 0 || self.win
+            puts `clear`
             self.show_display(self.draw_word, self.draw_guessed_letters, self.attempts_left)
             guess = self.get_next_letter
+
+            if guess == 'save'
+                puts `clear`
+                puts "#{BOLDS}See ya next time#{BOLDE}" 
+                return
+            end
 
             if !self.word.include?(guess)
                 self.attempts_left -= 1
@@ -153,7 +222,7 @@ class Game
             self.check_win            
 
             if self.win
-                puts
+                puts `clear`
                 puts "#{BOLDS}YOU WIN#{BOLDE}"
                 puts
                 puts self.draw_word
@@ -161,7 +230,7 @@ class Game
             end
             
         end
-
+        puts `clear`
         puts "Better luck next time! The word you were trying to guess was #{BOLDS}#{self.word}"
         
     end
